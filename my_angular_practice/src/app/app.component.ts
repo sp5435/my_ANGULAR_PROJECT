6f47a055dd4bd70f4cab57834e6909fd2131a581
SOURABH PATIL <patilsourabhsp1999@gmail.com>
2023-06-03 08:38:42 +0530

    ANGULAR PROJECT CREATED

--- my_angular_practice/src/app/app.component.ts
+++ my_angular_practice/src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my_angular_practice';
+  title = 'ANGULAR_PROJECT';
+ //propertyName :datatype = value/data;
+ //testcase1  
+  firstName : string = 'sourabh';  
+
+  //testcase 2
+  firstName1 = 'sourabh';
+  firstName2! : string  ;
+  surName2 : any;
+  surName : any = true;
+  formTitle : string = 'Login Form...';
+  studentName = 'sourabh';
+  amount = '5600';
+  isFavCity = false;
+  myName = "Om";
+  gender = false;
+  isDisabled = true;
+  eventBindingData = 60000;
+  twoWayBindingTest = 96565;
+
+   testingInterpolation( x : number, y : number ){
+  
+      return x + y;
+  }
+
+  testingEventBinding(){
+    this.eventBindingData = 50000;
+    console.log(this.eventBindingData);
+    
+  }
+  test(){
+    console.log(this.twoWayBindingTest);
+  }
 }
+
+
+
+
+
+  
+  
+
+// data binding in angular
+//tow categouries
+//1. one way data binding: 
+// I. string interpolation{{}} data transfar karna ya baind karna
+// II. property binding :[]
+// Iv. event binding :()
+//2. two way data binding:[()]
